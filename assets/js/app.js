15a4c393ca4e98e5423ce46ac0863b88f098ae4b
thibauttaglialatela <74903726+thibauttaglialatela@users.noreply.github.com>
2025-02-12 14:13:17 +0000

    Deploying to gh-pages from @ thibauttaglialatela/taglialatela-portfolio-html@42fa755ac396a9e1b5ae318f58113f8e55af8e93 🚀

---
+++ assets/js/app.js
@@ -0,0 +1,32 @@
+const scrollUp = document.querySelector('.scroll-up');
+
+scrollUp.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        left: 0,
+        behavior: 'smooth'
+    })
+});
+
+//Nav hamburger menu selections
+
+const burger = document.querySelector('#burger-menu');
+const ul = document.querySelector('nav ul');
+const nav = document.querySelector('nav');
+
+burger.addEventListener("click", function() {
+    ul.classList.toggle("show");
+});
+
+//hide menu after clicking on a link
+const navLinkArray = document.querySelectorAll('.nav-link');
+
+navLinkArray.forEach(element => {
+    element.addEventListener("click", () => {
+    ul.classList.remove("show");
+    })
+});
+
+//get current year
+let currentYear = new Date().getFullYear();
+document.querySelector('#currentYear').textContent = currentYear;
